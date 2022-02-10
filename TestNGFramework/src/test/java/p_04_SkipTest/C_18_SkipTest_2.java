dcbfd3de855b0c64814b2c9e5e10c23dd93e5333
BADHAN <BADHAN@BADHAN-PC>
2022-02-11 00:35:53 +0600

    Skip Test incorporated

---
+++ TestNGFramework/src/test/java/p_04_SkipTest/C_18_SkipTest_2.java
@@ -0,0 +1,45 @@
+package p_04_SkipTest;
+
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+public class C_18_SkipTest_2 {
+	
+	boolean dataCreation = false;
+	
+	//Not enabled
+	@Test(enabled=false)
+	public void notEnabled() {
+		
+		System.out.println("Test case skipped due to not enabled.");
+	}
+	
+	
+	//Force Skip
+	@Test
+	public void forceSkip() {
+		
+		System.out.println("Test case skipped forcefully.");
+		
+		throw new SkipException("Forcefully skipped.");
+	}
+	
+	//Conditional Skip
+	@Test
+	public void conditionalSkip() {
+				
+		if(dataCreation==true) {
+			
+			System.out.println("Test case not skipped.");
+		}
+		
+		else {
+			
+			System.out.println("Test case skip based on condition.");
+			
+			throw new SkipException("Conditional skip.");
+		}
+		
+	}
+
+}
